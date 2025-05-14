e1004c8d0aca7475fb39e0a9368afd57156ab765
lorinsteiner <lorin.steiner@bluewin.ch>
2025-05-14 15:17:36 +0200

    create MongoDB container and its integration in API

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,24 @@
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Minimal API Version 1.0");
+
+app.MapGet("/check", () => {
+    try {
+        string connectionUri = "mongodb://gbs:geheim@mongodb:27017";
+        var client = new MongoClient(connectionUri);
+
+        var databaseNames = client.ListDatabaseNames().ToList();
+
+        return Results.Ok(new {
+            Message = "db access ok",
+            Databases = databaseNames
+        });
+    } catch(Exception ex) {
+        return Results.Problem($"db access not ok: {ex.Message}");
+    }
+});
 
 app.Run();
